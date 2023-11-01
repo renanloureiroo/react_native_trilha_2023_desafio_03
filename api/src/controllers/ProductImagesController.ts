68c3f875aa78f85b047711e4e83324ade599116c
Renan Pereira Loureiro <renan.pereiral@sonda.com>
2023-11-01 16:38:30 -0300

    feat: tests data

--- api/src/controllers/ProductImagesController.ts
+++ api/src/controllers/ProductImagesController.ts
@@ -10,35 +10,43 @@ export class ProductImagesController {
   async create(request: Request, response: Response) {
     const userId = request.user.id;
     const productId = request.body.product_id;
-    const productFiles = request.files
+    const productFiles = request.files;
 
+    console.log("REQUEST", request.body);
     const diskStorage = new DiskStorage();
 
     const product = await prisma.products.findUnique({
       where: {
         id: productId,
-      }
-    })
+      },
+    });
 
-    if (!productFiles || productFiles?.length === 0 || !Array.isArray(productFiles)) {
-      throw new AppError("É obrigatório o envio de imagens.")
+    if (
+      !productFiles ||
+      productFiles?.length === 0 ||
+      !Array.isArray(productFiles)
+    ) {
+      throw new AppError("É obrigatório o envio de imagens.");
     }
-    
+
     if (!product) {
       productFiles.forEach(async (productFilename) => {
-        await diskStorage.deleteFile(productFilename.path)
-      })
+        await diskStorage.deleteFile(productFilename.path);
+      });
       throw new AppError("Produto não encontrado.", 404);
     }
 
     if (product.user_id !== userId) {
       productFiles.forEach(async (productFilename) => {
-        await diskStorage.deleteFile(productFilename.path)
-      })
-      throw new AppError("Somente o dono do anúncio pode gerenciar as imagens do produto.", 401);
+        await diskStorage.deleteFile(productFilename.path);
+      });
+      throw new AppError(
+        "Somente o dono do anúncio pode gerenciar as imagens do produto.",
+        401
+      );
     }
 
-    const productImages: ProductImages[] = []
+    const productImages: ProductImages[] = [];
 
     if (Array.isArray(productFiles)) {
       for (const productFilename of productFiles) {
@@ -46,11 +54,11 @@ export class ProductImagesController {
         const productImage = await prisma.productImages.create({
           data: {
             path: filename,
-            product_id: productId
-          }
-        })
+            product_id: productId,
+          },
+        });
 
-        productImages.push(productImage)
+        productImages.push(productImage);
       }
     } else {
       throw new AppError("Formato de formulário de imagens inválido.");
@@ -66,46 +74,51 @@ export class ProductImagesController {
     const diskStorage = new DiskStorage();
 
     if (productImagesIds.length === 0) {
-      throw new AppError("É obrigatório o envio dos ids das imagens a serem deletadas.")
+      throw new AppError(
+        "É obrigatório o envio dos ids das imagens a serem deletadas."
+      );
     }
 
     const productImages = await prisma.productImages.findMany({
       where: {
         id: {
-          in: productImagesIds
-        }
+          in: productImagesIds,
+        },
       },
       include: {
         product: {
           select: {
-            user_id: true
-          }
-        }
-      }
-    })
-    
+            user_id: true,
+          },
+        },
+      },
+    });
+
     if (productImages.length !== productImagesIds.length) {
       throw new AppError("Imagens não encontradas.", 404);
     }
 
     for (const productImage of productImages) {
       if (productImage.product.user_id !== userId) {
-        throw new AppError("Somente o dono do anúncio pode gerenciar as imagens do produto.", 401);
+        throw new AppError(
+          "Somente o dono do anúncio pode gerenciar as imagens do produto.",
+          401
+        );
       }
     }
 
     for (const productImage of productImages) {
-      await diskStorage.deleteFile(productImage.path)
+      await diskStorage.deleteFile(productImage.path);
     }
 
     await prisma.productImages.deleteMany({
       where: {
         id: {
-          in: productImagesIds
-        }
-      }
-    })
+          in: productImagesIds,
+        },
+      },
+    });
 
-    return response.status(204).json()
+    return response.status(204).json();
   }
 }
--- api/src/controllers/ProductsController.ts
+++ api/src/controllers/ProductsController.ts
@@ -7,7 +7,7 @@ import { AppError } from "../utils/AppError";
 
 export class ProductsController {
   async show(request: Request, response: Response) {
-    const productId = request.params.id
+    const productId = request.params.id;
 
     const product = await prisma.products.findUnique({
       where: {
@@ -17,44 +17,56 @@ export class ProductsController {
         product_images: {
           select: {
             path: true,
-            id: true
-          }
+            id: true,
+          },
         },
         payment_methods: {
           select: {
             key: true,
-            name: true
-          }
+            name: true,
+          },
         },
         user: {
           select: {
             avatar: true,
             name: true,
-            tel: true
-          }
-        }
-      }
-    })
+            tel: true,
+          },
+        },
+      },
+    });
 
     if (!product) {
-      throw new AppError('Produto não encontrado.', 404);
+      throw new AppError("Produto não encontrado.", 404);
     }
 
     return response.json(product);
   }
 
   async index(request: Request, response: Response) {
-    const userId = request.user.id
+    const userId = request.user.id;
 
-    const is_new = request.query.is_new === undefined ? undefined : request.query.is_new === 'true'
-    const accept_trade = request.query.accept_trade === undefined ? undefined : request.query.accept_trade === 'true'
-    const query = typeof request.query.query === 'string' ? request.query.query : undefined
-    const payment_methods = request.query.payment_methods === undefined ? undefined : typeof request.query.payment_methods === 'string' ? new Array(request.query.payment_methods) : request.query.payment_methods as string[];
-    
+    const is_new =
+      request.query.is_new === undefined
+        ? undefined
+        : request.query.is_new === "true";
+    const accept_trade =
+      request.query.accept_trade === undefined
+        ? undefined
+        : request.query.accept_trade === "true";
+    const query =
+      typeof request.query.query === "string" ? request.query.query : undefined;
+    const payment_methods =
+      request.query.payment_methods === undefined
+        ? undefined
+        : typeof request.query.payment_methods === "string"
+        ? new Array(request.query.payment_methods)
+        : (request.query.payment_methods as string[]);
+
     const products = await prisma.products.findMany({
       where: {
         user_id: {
-          not: userId
+          not: userId,
         },
         is_active: true,
         is_new,
@@ -62,13 +74,16 @@ export class ProductsController {
         payment_methods: {
           some: {
             key: {
-              in: Array.isArray(payment_methods) && payment_methods.length > 0 ? payment_methods : undefined
-            }
-          }
+              in:
+                Array.isArray(payment_methods) && payment_methods.length > 0
+                  ? payment_methods
+                  : undefined,
+            },
+          },
         },
         name: {
-          contains: query
-        }
+          contains: query,
+        },
       },
       select: {
         id: true,
@@ -79,71 +94,73 @@ export class ProductsController {
         product_images: {
           select: {
             path: true,
-            id: true
-          }
+            id: true,
+          },
         },
         payment_methods: {
           select: {
             key: true,
-            name: true
-          }
+            name: true,
+          },
         },
         user: {
           select: {
-            avatar: true
-          }
-        }
-      }
-    })
+            avatar: true,
+          },
+        },
+      },
+    });
 
     return response.json(products);
   }
 
   async create(request: Request, response: Response) {
-    const { 
-      name,
-      description,
-      is_new,
-      price,
-      accept_trade,
-      payment_methods
-    } = request.body
-    const userId = request.user.id
+    const { name, description, is_new, price, accept_trade, payment_methods } =
+      request.body;
+    const userId = request.user.id;
 
+    console.log("REQUEST_BODY", request.body);
+    console.log("REQUEST_METHOD", request.method);
     if (!name) {
-      throw new AppError("O nome do produto é obrigatório.")
+      throw new AppError("O nome do produto é obrigatório.");
     }
 
     if (!description) {
-      throw new AppError("A descrição do produto é obrigatória.")
+      throw new AppError("A descrição do produto é obrigatória.");
     }
 
-    if (typeof is_new === 'string' ? !is_new : typeof is_new === undefined) {
-      throw new AppError("A condição (novo ou usado) do produto é obrigatória.")
+    if (typeof is_new === "string" ? !is_new : typeof is_new === undefined) {
+      throw new AppError(
+        "A condição (novo ou usado) do produto é obrigatória."
+      );
     }
 
     if (!price) {
-      throw new AppError("O preço do produto é obrigatório.")
+      throw new AppError("O preço do produto é obrigatório.");
     }
 
-    if (typeof accept_trade === 'string' ? !accept_trade : typeof accept_trade === undefined) {
-      throw new AppError("A condição de troca é obrigatória.")
+    if (
+      typeof accept_trade === "string"
+        ? !accept_trade
+        : typeof accept_trade === undefined
+    ) {
+      throw new AppError("A condição de troca é obrigatória.");
     }
 
     if (!Array.isArray(payment_methods) || payment_methods.length === 0) {
-      throw new AppError("Os métodos de pagamento são obrigatórios")
+      throw new AppError("Os métodos de pagamento são obrigatórios");
     }
 
     const arePaymentsMethodsValid = await prisma.paymentMethods.findMany({
       where: {
         key: {
-          in: payment_methods
-        }
-      }
-    })
+          in: payment_methods,
+        },
+      },
+    });
 
     if (arePaymentsMethodsValid.length !== payment_methods.length) {
-      throw new AppError("Um ou mais métodos de pagamento são inválidos.")
+      throw new AppError("Um ou mais métodos de pagamento são inválidos.");
     }
 
     const product = await prisma.products.create({
@@ -153,29 +170,23 @@ export class ProductsController {
         is_new,
         name,
         payment_methods: {
-          connect: payment_methods.map(payment_method => ({
-            key: payment_method
-          }))
+          connect: payment_methods.map((payment_method) => ({
+            key: payment_method,
+          })),
         },
         price,
-        user_id: userId
-      }
-    })
+        user_id: userId,
+      },
+    });
 
     return response.status(201).json(product);
   }
 
   async update(request: Request, response: Response) {
-    const {
-      name,
-      description,
-      is_new,
-      price,
-      accept_trade,
-      payment_methods 
-    } = request.body
-    const userId = request.user.id
-    const productId = request.params.id
+    const { name, description, is_new, price, accept_trade, payment_methods } =
+      request.body;
+    const userId = request.user.id;
+    const productId = request.params.id;
 
     const product = await prisma.products.findUnique({
       where: {
@@ -184,77 +195,86 @@ export class ProductsController {
       include: {
         payment_methods: {
           select: {
-            key: true
-          }
-        }
-      }
-    })
+            key: true,
+          },
+        },
+      },
+    });
 
     if (!product) {
-      throw new AppError("O produto a ser atualizado não foi encontrado.", 404)
+      throw new AppError("O produto a ser atualizado não foi encontrado.", 404);
     }
 
     if (product.user_id !== userId) {
-      throw new AppError("O usuário não tem permissão para atualizar o produto", 401)
+      throw new AppError(
+        "O usuário não tem permissão para atualizar o produto",
+        401
+      );
     }
 
-    let newPaymentsMethods: string[] | undefined
-    let oldPaymentsMethods: string[] | undefined
+    let newPaymentsMethods: string[] | undefined;
+    let oldPaymentsMethods: string[] | undefined;
 
     if (Array.isArray(payment_methods)) {
       if (payment_methods.length === 0) {
-        throw new AppError("O produto precisa ter pelo menos 1 método de pagamento.")
+        throw new AppError(
+          "O produto precisa ter pelo menos 1 método de pagamento."
+        );
       }
 
       const arePaymentsMethodsValid = await prisma.paymentMethods.findMany({
         where: {
           key: {
-            in: payment_methods
-          }
-        }
-      })
-  
+            in: payment_methods,
+          },
+        },
+      });
+
       if (arePaymentsMethodsValid.length !== payment_methods.length) {
-        throw new AppError("Um ou mais métodos de pagamento são inválidos.")
+        throw new AppError("Um ou mais métodos de pagamento são inválidos.");
       }
 
-      oldPaymentsMethods = product.payment_methods.filter(payment_method => !payment_methods.includes(payment_method.key)).map(payment_method => payment_method.key)
-      newPaymentsMethods = payment_methods.filter(payment_method => !product.payment_methods.includes(payment_method))
+      oldPaymentsMethods = product.payment_methods
+        .filter(
+          (payment_method) => !payment_methods.includes(payment_method.key)
+        )
+        .map((payment_method) => payment_method.key);
+      newPaymentsMethods = payment_methods.filter(
+        (payment_method) => !product.payment_methods.includes(payment_method)
+      );
     }
 
-    product.accept_trade = accept_trade ?? product.accept_trade
-    product.description = description ?? product.description
-    product.is_new = is_new ?? product.is_new
-    product.name = name ?? product.name
-    product.price = price ?? product.price
-    product.updated_at = new Date()
+    product.accept_trade = accept_trade ?? product.accept_trade;
+    product.description = description ?? product.description;
+    product.is_new = is_new ?? product.is_new;
+    product.name = name ?? product.name;
+    product.price = price ?? product.price;
+    product.updated_at = new Date();
 
     await prisma.products.update({
       data: {
         ...product,
         payment_methods: {
-          disconnect: oldPaymentsMethods?.map(payment_method => ({
-            key: payment_method
+          disconnect: oldPaymentsMethods?.map((payment_method) => ({
+            key: payment_method,
           })),
-          connect: newPaymentsMethods?.map(payment_method => ({
-            key: payment_method
-          }))
-        }
+          connect: newPaymentsMethods?.map((payment_method) => ({
+            key: payment_method,
+          })),
+        },
       },
       where: {
-        id: productId
-      }
-    })
+        id: productId,
+      },
+    });
 
     return response.status(204).json();
   }
 
   async patch(request: Request, response: Response) {
-    const {
-      is_active
-    } = request.body
-    const userId = request.user.id
-    const productId = request.params.id
+    const { is_active } = request.body;
+    const userId = request.user.id;
+    const productId = request.params.id;
 
     const product = await prisma.products.findUnique({
       where: {
@@ -263,35 +283,38 @@ export class ProductsController {
       include: {
         payment_methods: {
           select: {
-            key: true
-          }
-        }
-      }
-    })
+            key: true,
+          },
+        },
+      },
+    });
 
     if (!product) {
-      throw new AppError("O produto a ser atualizado não foi encontrado.", 404)
+      throw new AppError("O produto a ser atualizado não foi encontrado.", 404);
     }
 
     if (product.user_id !== userId) {
-      throw new AppError("O usuário não tem permissão para atualizar o produto", 401)
+      throw new AppError(
+        "O usuário não tem permissão para atualizar o produto",
+        401
+      );
     }
 
     await prisma.products.update({
       data: {
-        is_active
+        is_active,
       },
       where: {
-        id: productId
-      }
-    })
+        id: productId,
+      },
+    });
 
     return response.status(204).json();
   }
 
   async delete(request: Request, response: Response) {
-    const userId = request.user.id
-    const productId = request.params.id
+    const userId = request.user.id;
+    const productId = request.params.id;
 
     const diskStorage = new DiskStorage();
 
@@ -302,29 +325,32 @@ export class ProductsController {
       include: {
         product_images: {
           select: {
-            path: true
-          }
-        }
-      }
-    })
+            path: true,
+          },
+        },
+      },
+    });
 
     if (!product) {
-      throw new AppError("O produto a ser removido não foi encontrado.", 404)
+      throw new AppError("O produto a ser removido não foi encontrado.", 404);
     }
 
     if (product.user_id !== userId) {
-      throw new AppError("O usuário não tem permissão para atualizar o produto.", 401)
+      throw new AppError(
+        "O usuário não tem permissão para atualizar o produto.",
+        401
+      );
     }
 
     for (const productImage of product.product_images) {
-      await diskStorage.deleteFile(productImage.path)
+      await diskStorage.deleteFile(productImage.path);
     }
 
     await prisma.products.delete({
       where: {
-        id: productId
-      }
-    })
+        id: productId,
+      },
+    });
 
     return response.status(204).json();
   }
